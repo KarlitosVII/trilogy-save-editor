a09d0b5f0037d31c6c882d7cfc9aada62ca77357
KarlitosVII <7722685+KarlitosVII@users.noreply.github.com>
2021-06-03 22:02:42 +0200

    Added changelog file + read it directly in the application

---
+++ CHANGELOG.md
@@ -0,0 +1,74 @@
+## 1.8.3
+- Support for Cyrillic and Japanese characters
+- Fixed lag when moving the window (caused by frame limit in previous version)
+- Reduced CPU usage in some cases (particularly noticeable when opening `Head Morph > Lod0`)
+
+## 1.8.2
+- Fixed `add` button not working for War Assets and some other lists
+- 60 fps limit for those who have a display above 60Hz. (May reduce CPU / GPU usage on some configs)
+- Various small fixes
+
+## 1.8.1
+- Fixed `invalid value: '6', expected variant index 0 <= i < 6`
+- Open save dialog in the same directory of the save
+
+## 1.8.0
+- ME1: Renamed salvage to omnigel
+- ME1LE: Added player talent points and some stats
+- ME1LE: Added squad (with talents, talent points, inventory and stats)
+- ME1LE: Added map name, location and rotation
+- ME1LE: Added difficulty, character creation date and player controller
+- When creating backup, copy the old save instead of renaming it. This should fix the game loading old save.
+
+## 1.7.2
+- Swapped ME1LE medigel and grenades
+- Fixed some ME3 bonus power names
+- Unhide some raw data that can be used for modding purpose (Debug name, placeables, doors, etc.)
+
+## 1.7.1
+- Changed dialog library, I hope it will fix `Open` dialog not opening
+
+## 1.7.0
+- Added ME1LE resources (credits, grenades, medigel, salvage) and face code
+- Added ME1LE raw player inventory
+- Changed backend (again) with a more robust one, it will choose a supported backend on your system (Vulkan, DX11/12, etc.)
+
+## 1.6.1
+- Changed backend from OpenGL to Vulkan, I hope it will fix GPU and OpenGL related bugs
+
+## 1.6.0
+- Added ME1LE Level and Current XP
+- Added ME1LE Raw talents
+- Converted raw texts to title case for better readability
+
+## 1.5.0
+- Added ME1LE `General` tab with basic information such as Name, Gender, Origin, Notoriety and Morality
+- Added ME1LE `Head Morph` tab with Import / Export and raw data
+
+## 1.3.2
+- New UNC plots in ME1 (thanks to Yggge)
+- Added clarification for editing ME1 plot in ME2 save
+
+## 1.3.1
+- Fix ME1LE `unexpected end of file...` error for some people
+
+## 1.3.0
+- Initial Mass Effect 1 Legendary support (only plot)
+
+## 1.2.0
+- ME2/3 Legendary support
+
+## 1.1.2
+- Changing ME2/3 origin / notoriety will update ME1's
+- Changing ME3 gender will change Loco / Lola plot corresponding to new gender
+
+## 1.1.1
+- High CPU usage fix
+
+## 1.1.0
+- HiDPI fix
+- Possibility to modify previously read-only ME1 raw strings
+- Minor fixes
+
+## 1.0.0
+- Initial release
--- src/gui/changelog.rs
+++ src/gui/changelog.rs
@@ -4,95 +4,27 @@ use lazy_static::lazy_static;
 use super::Gui;
 
 lazy_static! {
-    static ref CHANGELOGS: Vec<(&'static str, Vec<&'static str>)> = vec![
-        (env!("CARGO_PKG_VERSION"), // "1.8.2",
-        vec![
-            "Fixed `add` button not working for War Assets and some other lists",
-            "60 fps limit for those who have a display above 60Hz. (May reduce CPU / GPU usage on some configs)",
-            "Various small fixes",
-        ]),
-        ("1.8.1",
-        vec![
-            "Fixed `invalid value: '6', expected variant index 0 <= i < 6`",
-            "Open save dialog in the same directory of the save",
-        ]),
-        ("1.8.0",
-        vec![
-            "ME1: Renamed salvage to omnigel",
-            "ME1LE: Added player talent points and some stats",
-            "ME1LE: Added squad (with talents, talent points, inventory and stats)",
-            "ME1LE: Added map name, location and rotation",
-            "ME1LE: Added difficulty, character creation date and player controller",
-            "When creating backup, copy the old save instead of renaming it. This should fix the game loading old save.",
-        ]),
-        ("1.7.2",
-        vec![
-            "Swapped ME1LE medigel and grenades",
-            "Fixed some ME3 bonus power names",
-            "Unhide some raw data that can be used for modding purpose (Debug name, placeables, doors, etc.)",
-        ]),
-        ("1.7.1",
-        vec![
-            "Changed dialog library, I hope it will fix `Open` dialog not opening",
-        ]),
-        ("1.7.0",
-        vec![
-            "Added ME1LE resources (credits, grenades, medigel, salvage) and face code",
-            "Added ME1LE raw player inventory",
-            "Changed backend (again) with a more robust one, it will choose a supported backend on your system (Vulkan, DX11/12, etc.)",
-        ]),
-        ("1.6.1",
-        vec![
-            "Changed backend from OpenGL to Vulkan, I hope it will fix GPU and OpenGL related bugs",
-        ]),
-        ("1.6.0",
-        vec![
-            "Added ME1LE Level and Current XP",
-            "Added ME1LE Raw talents",
-            "Converted raw texts to title case for better readability",
-        ]),
-        ("1.5.0",
-        vec![
-            "Added ME1LE `General` tab with basic information such as Name, Gender, Origin, Notoriety and Morality",
-            "Added ME1LE `Head Morph` tab with Import / Export and raw data",
-        ]),
-        ("1.3.2",
-        vec![
-            "New UNC plots in ME1 (thanks to Yggge)",
-            "Added clarification for editing ME1 plot in ME2 save",
-        ]),
-        ("1.3.1",
-        vec![
-            "Fix ME1LE `unexpected end of file...` error for some people",
-        ]),
-        ("1.3.0",
-        vec![
-            "Initial Mass Effect 1 Legendary support (only plot)",
-        ]),
-        ("1.2.0",
-        vec![
-            "ME2/3 Legendary support",
-        ]),
-        ("1.1.2",
-        vec![
-            "Changing ME2/3 origin / notoriety will update ME1's",
-            "Changing ME3 gender will change Loco / Lola plot corresponding to new gender",
-        ]),
-        ("1.1.1",
-        vec![
-            "High CPU usage fix",
-        ]),
-        ("1.1.0",
-        vec![
-            "HiDPI fix",
-            "Possibility to modify previously read-only ME1 raw strings",
-            "Minor fixes",
-        ]),
-        ("1.0.0",
-        vec![
-            "Initial release",
-        ]),
-    ];
+    static ref CHANGELOG: Vec<(&'static str, Vec<&'static str>)> = {
+        let file = include_str!("../../CHANGELOG.md");
+        let mut changelog = Vec::new();
+        let mut changes = Vec::new();
+        let mut version = "";
+
+        for line in file.split('\n') {
+            if let Some((prefix, text)) = line.split_once(' ') {
+                match prefix {
+                    "##" => version = text,
+                    "-" | "*" => changes.push(text),
+                    _ => {}
+                }
+            } else {
+                changelog.push((version, changes.clone()));
+                changes.clear();
+            }
+        }
+
+        changelog
+    };
 }
 
 impl<'ui> Gui<'ui> {
@@ -105,7 +37,7 @@ impl<'ui> Gui<'ui> {
         ui.separator();
 
         let mut first = true;
-        for (version, changes) in CHANGELOGS.iter() {
+        for (version, changes) in CHANGELOG.iter() {
             if let Some(_t) = self.begin_table(&im_str!("{}-table", version), 1) {
                 self.table_next_row();
                 if first {
